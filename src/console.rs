f9a80aa09c8a6d7185410c5f6bd14f51219c1313
evpeople <evpeople@gmail.com>
2022-01-07 07:02:27 -0800

    ✨ feat(src/console): success log

--- src/console.rs
+++ src/console.rs
@@ -1,13 +1,12 @@
 use crate::sbi::console_putchar;
 use core::fmt::{self, Write};
-// extern crate log;
 use log::{self, Level, LevelFilter, Log, Metadata, Record};
 struct Stdout;
 
 impl Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         for c in s.chars() {
-	console_putchar(c as usize)
+            console_putchar(c as usize)
         }
         Ok(())
     }
@@ -16,12 +15,30 @@ pub fn init() {
     static LOGGER: SimpleLogger = SimpleLogger;
     log::set_logger(&LOGGER).unwrap();
     log::set_max_level(match option_env!("LOG") {
-        Some("error") => LevelFilter::Error,
-        Some("warn") => LevelFilter::Warn,
-        Some("info") => LevelFilter::Info,
-        Some("debug") => LevelFilter::Debug,
-        Some("trace") => LevelFilter::Trace,
-        _ => LevelFilter::Off,
+        Some("error") => {
+            print(format_args!("Error"));
+            LevelFilter::Error
+        }
+        Some("warn") => {
+            print(format_args!("Warn"));
+            LevelFilter::Warn
+        }
+        Some("info") => {
+            print(format_args!("Info"));
+            LevelFilter::Info
+        }
+        Some("debug") => {
+            print(format_args!("Debug"));
+            LevelFilter::Debug
+        }
+        Some("trace") => {
+            print(format_args!("Trace"));
+            LevelFilter::Trace
+        }
+        _ => {
+            print(format_args!("d4d"));
+            LevelFilter::Off
+        }
     });
 }
 
@@ -41,10 +58,8 @@ macro_rules! println {
 		$crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
 	}
 }
-// "\x1b[31m hello world\x1
 
-pub struct SimpleLogger;
-// fn print_in_color(args:)
+struct SimpleLogger;
 impl Log for SimpleLogger {
     fn enabled(&self, _metadata: &Metadata) -> bool {
         true
@@ -53,19 +68,10 @@ impl Log for SimpleLogger {
         if !self.enabled(record.metadata()) {
             return;
         }
-	print_in_color(format_args!("{}",record.args()),level_to_color_code(record.level()));
-	// print_in_color("ds",2);
-        // print_in_color(
-        //     format_args!(
-        //         "[{:>5}][{},-] {}\n",
-        //         record.level(),
-        //         crate::arch::cpu::id(),
-        //         record.args()
-        //     ),
-        //     level_to_color_code(record.level()),
-        // );
-        //}
-	
+        print_in_color(
+            format_args!("{}", record.args()),
+            level_to_color_code(record.level()),
+        );
     }
     fn flush(&self) {}
 }
@@ -85,14 +91,5 @@ macro_rules! with_color {
 }
 
 fn print_in_color(args: fmt::Arguments, color_code: u8) {
-//     use crate::core::arch::io;
-//     let _guard = LOG_LOCK.lock();
-//     print!(with_color!(args, color_code));
-Stdout.write_fmt(with_color!(args, color_code)).unwrap();
+    Stdout.write_fmt(with_color!(args, color_code)).unwrap();
 }
-
-// pub fn print(args: fmt::Arguments) {
-// //     use crate::core::arch::io;
-// //     let _guard = LOG_LOCK.lock();
-// //     io::putfmt(args);
-// }
