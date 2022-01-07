6843048d3eb7b03e9561d86d679f9af3fbf577d8
evpeople <evpeople@gmail.com>
2022-01-07 05:38:45 -0800

    ✨ feat(log): fail log

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,38 @@
 mod console;
 mod lang_items;
 mod sbi;
-use  core::arch::global_asm;
+use core::arch::global_asm;
+use log::{error,LevelFilter,trace};
+
+// use console::init;
+// use console::SimpleLogger;
+// use log::info;
 
 global_asm!(include_str!("entry.asm"));
-
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    println!("hello rcore");
+    log::set_max_level(LevelFilter::Error);
+    println!("hello rcorefdsfds");
+    // console::init();
+    trace!("ddsadsaasdasdas");
+    // console::SimpleLogger.log(Record{});
+    // with_color!("w")
+    // print_in_color(format_args!("dsdsafadgdf"),31);
+    // init();    
+    // info!("dsasdsa");
+    // SimpleLogger;
     panic!("Shutdown machine!");
     loop {}
 }
-fn clear_bss(){
-    extern "C"{
+fn clear_bss() {
+    extern "C" {
         fn sbss();
         fn ebss();
     }
     //first find sbss and ebss ,then zero them,it translate to uszie
-    ( sbss as usize..ebss as usize).for_each(|a|{
-        unsafe{(a as *mut u8).write_volatile(0)}
-    });
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
 // fn main() {
 // //    println!("Hello, world!");
