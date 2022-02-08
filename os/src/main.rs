aa3f9defc66b0bd55a99b907ebf0075801d8eb71
evpeople <evpeople@gmail.com>
2022-02-08 05:38:02 -0800

    ✨ feat(batch): import two stack

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,28 +1,39 @@
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+
+use core::arch::global_asm;
+
 #[macro_use]
 mod console;
 mod lang_items;
 mod sbi;
-use core::arch::global_asm;
+mod syscall;
+mod trap;
+mod batch;
+mod sync;
 use log::{error, info, trace, warn, LevelFilter};
 
 global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
 
-    console::init();
-    log::set_max_level(LevelFilter::Error);
-    error!("sad");
-    warn!("21432");
-    println!("hello rcorefdsfds");
+    println!("[kernel] Hello, world!");
+    trap::init();
+    batch::init();
+    batch::run_next_app();
+    // console::init();    
+    // log::set_max_level(LevelFilter::Error);
+    // error!("sad");
+    // warn!("21432");
+    // println!("hello rcorefdsfds");
 
-    info!("123345ddsadsaasdasdas");
+    // info!("123345ddsadsaasdasdas");
 
-    panic!("Shutdown machine!");
+    // panic!("Shutdown machine!");
     loop {}
 }
 fn clear_bss() {
